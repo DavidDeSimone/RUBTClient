23141c2593e5e3d09084ab94239e0d85e316e954
Danny <KDavis275@gmail.com>
2014-10-08 03:47:34 -0400

    Added Message Classes

---
+++ src/com/ddsc/utils/HaveMessage.java
@@ -0,0 +1,15 @@
+
+public class HaveMessage extends Message{
+
+	private int piece_index;
+	
+	public HaveMessage(int piece_index){
+		super(5, Message.t_have);
+		this.piece_index = piece_index;
+	}
+	
+	public int getPieceIndex(){
+		return piece_index;
+	}
+		
+}
---
+++ src/com/ddsc/utils/RequestMessage.java
@@ -0,0 +1,43 @@
+
+public class RequestMessage extends Message {
+	
+	
+	private int piece_index;
+	private int block_offset;
+	private int block_length;
+	
+	public RequestMessage(int piece_index, int block_offset, int block_length) {
+		super(13, Message.t_request);
+		this.piece_index = piece_index;
+		this.block_offset = block_offset;
+		this.block_length = block_length;
+		
+	}
+	
+	public int getPieceIndex() {
+		return piece_index;
+	}
+	
+	public int getBlockOffset() {
+		return block_offset;
+	}
+	
+	public int getBlockLength() {
+		return block_length;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Request (").append(this.piece_index).append(", ").append(this.block_offset)
+				.append(", ").append(this.block_length).append(')');
+		return sb.toString();
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
